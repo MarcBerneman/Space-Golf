56ac3f732cbd9ac0bd5ab6d31fd4019d0917ab33
MarciBE <marcus.berneman@gmail.com>
2016-05-05 22:13:15 +0200

    changes

--- src/spelelementen/MainFrame.java
+++ src/spelelementen/MainFrame.java
@@ -4,11 +4,11 @@ import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
 public class MainFrame extends JFrame {
-	public final static double G = 2500; // Gravitatieconstante
+	public final static double G = 5000; // Gravitatieconstante
 	public final static double µ = 0.45; // Wrijvingscoëfficient
-	public final static double COR = 0.75; // Coëfficient of Restitution
-	public final static double DeltaT = 0.005;
-	public final static double MOUSE_SPEED_COEFFICIENT = 1.25; 
+	public final static double COR = 0.55; // Coëfficient of Restitution
+	public final static double DeltaT = 0.01;
+	public final static double MOUSE_SPEED_COEFFICIENT = 2.50; 
 	public final static double MINIMAL_AVERAGE_MOVEMENT = 1.5;
 
 	public MainFrame() {
